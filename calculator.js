2975a39d6496ed0e5aa79070bc2f84c4d07dbd06
horlaide22 <hartoke2sekinat@gmail.com>
2023-02-15 16:00:55 +0100

    class-activity

--- calculator.js
+++ calculator.js
@@ -1,15 +1,20 @@
 export function sum(a, b) {
-
+    let sumAll= a+b
+    return sumAll
 }
 
 export function subtract(a, b) {
+    let subtractAll= a-b
+    return subtractAll
 
 }
 
 export function multiply(a, b) {
-
+    let multiplyAll= a*b
+    return multiplyAll
 }
 
 export function divide(a, b) {
-
+    let divideAll= a/b
+    return divideAll
 }
