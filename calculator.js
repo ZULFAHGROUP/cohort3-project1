2a2e593d6ac15e87eb1596c0edba962fac68f4af
aneyobobo <gbemineeyi@yahoo.com>
2023-02-15 10:23:28 -0500

    my calculator

--- calculator.js
+++ calculator.js
@@ -1,15 +1,24 @@
 export function sum(a, b) {
+    let wale = a + b
+    return wale
 
 }
 
 export function subtract(a, b) {
+    let niyi = a - b
+    return niyi 
 
 }
 
 export function multiply(a, b) {
+    let mo = a * b
+    return mo
 
 }
 
 export function divide(a, b) {
+    let yinka = a / b
+    return yinka
+
 
 }
