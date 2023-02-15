58553940def355cc81c4dd011f1265405a4bb31e
Dapious99 <dapious99@gmail.com>
2023-02-15 15:58:05 -0800

    calculator

--- calculator.js
+++ calculator.js
@@ -1,15 +1,19 @@
 export function sum(a, b) {
-
+    let result = a + b
+    return result
 }
 
 export function subtract(a, b) {
-
+    let result = a - b
+    return result
 }
 
 export function multiply(a, b) {
-
+    let result = a * b
+    return result
 }
 
 export function divide(a, b) {
-
+    let result = a / b
+    return result
 }
