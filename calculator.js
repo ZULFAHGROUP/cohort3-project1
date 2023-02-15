07f856f3cf9ab47ebdd00a73e95d5e4a7d3f50ba
Shukurat Abiodun Ashiru <101349208+Shukuratgithubs@users.noreply.github.com>
2023-02-15 16:29:24 +0100

    calculator function

--- calculator.js
+++ calculator.js
@@ -1,15 +1,20 @@
 export function sum(a, b) {
-
+let sum =a+b;
+return sum
 }
 
 export function subtract(a, b) {
-
+let sub =a-b
+return sub
 }
 
 export function multiply(a, b) {
-
+let exponetial = a*b
+return exponetial
 }
 
 export function divide(a, b) {
-
+let div = a/b
+return div
 }
+~
